dd512c9dfd9a2a8620abcc8405d515573355ecb9
Tirth Mewada <tirthmewada12345@gmail.com>
2024-12-24 11:28:07 +0530

    Improved bulk and added button

--- src/app/dashboard/customers/page.jsx
+++ src/app/dashboard/customers/page.jsx
@@ -3,16 +3,51 @@ import React from 'react'
 import DataTable from './Table'
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 import { Breadcrumb, BreadcrumbEllipsis, BreadcrumbItem, BreadcrumbLink,BreadcrumbList,BreadcrumbSeparator,BreadcrumbPage } from '@/components/ui/breadcrumb'
 export default function CustomerPage() {
+
+
+
+  const router = useRouter();
+
+  const handleRedirect = () => {
+    router.push('/dashboard/customers/bulk'); // Replace '/newpage' with your desired route
+  };
+
+  const handleNewcust = () => {
+    router.push('/dashboard/customers/new'); // Replace '/newpage' with your desired route
+  };
+
   return (
    
     <div className="space-y-6 w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       
       <h1 className="text-2xl font-bold ">Manage Customers</h1>
       <div style={{ position: "relative", bottom:"35px",  width: "100%" }}>
-        <Link href="/dashboard/customers/new">
-        <Button style={{
+     
+        <Button 
+        onClick={handleRedirect}
+        style={{
+      position: "absolute",
+      top: "50%",
+      right: "250px",
+      transform: "translateY(-50%)",
+      padding: "10px 20px",
+      fontSize: "16px",
+      
+      border: "none",
+      borderRadius: "5px",
+      cursor: "pointer",
+
+      }}>+ Add Bulk Customer</Button>
+      
+       
+       
+        
+        <Button 
+        onClick={handleNewcust}
+          style={{
       position: "absolute",
       top: "50%",
       right: "20px",
@@ -24,7 +59,7 @@ export default function CustomerPage() {
       borderRadius: "5px",
       cursor: "pointer",
 
-      }}>+ Add New Customer</Button></Link>
+      }}>+ Add New Customer</Button>
       </div>
       <DataTable/>
     </div>
