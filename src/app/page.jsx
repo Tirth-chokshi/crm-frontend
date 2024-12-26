e80c9617067c8be242601283950bd88e4abbd103
Tirth Mewada <tirthmewada12345@gmail.com>
2024-12-26 12:23:44 +0530

    Added redirect to ligin direct

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,14 @@
-import Image from "next/image";
+
+"use client"
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        Hello world
-      </div>
-    </main>
-  );
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace('/login');
+  }, [router]);
+
+  return null;
 }
