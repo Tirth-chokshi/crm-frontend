ad56d283cc9bd724ea4095c78e132cb28736c762
Tirth Mewada <tirthmewada12345@gmail.com>
2024-12-30 16:41:17 +0530

    changes inj path from customer view page

--- src/app/dashboard/activity/view/[id]/page.jsx
+++ src/app/dashboard/activity/view/[id]/page.jsx
@@ -149,22 +149,15 @@ export default function ActivityView() {
       </div>
 
       {/* Back Button */}
-      <div className="mt-6 text-left">
+      <div className="mt-6 text-right">
         <Button
-          onClick={() => router.push(`/dashboard/customers/`)}
+          onClick={() => router.push(`/dashboard/customers/${activity.customer_id}`)}
           className="bg-blue-600 text-white hover:bg-blue-700"
         >
           Back to Customers 
         </Button>
       </div>
-      <div className="mt-6 text-right">
-        <Button
-          onClick={() => router.push("/dashboard/activity")}
-          className="bg-blue-600 text-white hover:bg-blue-700"
-        >
-          Back to Activities List 
-        </Button>
-      </div>
+   
     </div>
   );
 }
